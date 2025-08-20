800065cd3664613aaeecf526b86964e764ac725a
Tanmay Islam <tanmayislam101@gmail.com>
2025-08-20 22:57:52 +0600

    addd layout

---
+++ src/app/(authentication)/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AuthenticationLayout = ({children}) => {
+    return (
+        <div>
+            <h1>Authentication</h1>
+            {children}
+        </div>
+    );
+};
+
+export default AuthenticationLayout;
